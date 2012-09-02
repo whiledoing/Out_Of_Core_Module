bcf559ab907e93c6c277cba54a7cbce88a8d16af
whiledoing <whiledoing@sina.com>
2012-09-02 10:34:22 +0800

    change small format in qbigimagewidget.h
    Signed-off-by: whiledoing <whiledoing@sina.com>

--- ReadingBigImage/qbigimagewidget.h
+++ ReadingBigImage/qbigimagewidget.h
@@ -90,10 +90,13 @@ private:
 			try {
 
 				/* get the new image data */
-				if(!big_image->get_pixels_by_level_fast(img_current_level, start_row, start_col, img_rows, img_cols, img_data)) {
+				if(!big_image->get_pixels_by_level_fast(img_current_level, 
+					start_row, start_col, img_rows, img_cols, img_data)) {
 					init_para();
-					if(QMessageBox::Abort == QMessageBox::critical(this, "ReadingBigImage", 
-						QString::fromLocal8Bit("¶ÁÈ¡Í¼ÏñÊý¾ÝÊ§°Ü"), QMessageBox::Ok | QMessageBox::Abort, QMessageBox::Ok)) {
+					if(QMessageBox::Abort == QMessageBox::critical(this, 
+						"ReadingBigImage", 
+						QString::fromLocal8Bit("¶ÁÈ¡Í¼ÏñÊý¾ÝÊ§°Ü"), 
+						QMessageBox::Ok | QMessageBox::Abort, QMessageBox::Ok)) {
 						m_parent->close();
 					}
 
